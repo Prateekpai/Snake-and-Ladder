2e4d15d7b568427066387c4521d853f865acdfb4
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-06 18:11:29 +0530

    Welcome to Snake and Ladder Practice Program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+                Console.WriteLine("Welcome to the Snake and Ladder Game");
+
+        }
+    }
+
+}
