476c94ac0795db7ea541c799ef98f0ea5c91751a
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-09 09:14:19 +0530

    Player Rolls the Die

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace SnakeAndLadder
+{
+    public class UC2
+    {
+        public static void Main(string[] args)
+        {
+            int StartPosition = 0;
+            Random random = new Random();
+            int DiceNo = random.Next(1, 7);
+
+            {
+                Console.WriteLine("Welcome to the Snake and Ladder Game");
+                Console.WriteLine("....................................");
+                Console.WriteLine("The Start Position of The Player is : {0}", StartPosition);
+                Console.WriteLine("The Dice Number is : {0}",DiceNo);   
+            }
+        }
+    }
+
+}
