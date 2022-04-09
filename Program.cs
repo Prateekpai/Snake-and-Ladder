8d4167fa92c4f22f33b22a01bfbb1655101454f0
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-09 09:12:04 +0530

    Player Starts with Position Zero

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace SnakeAndLadder
+{
+    public class UC1
+    {
+        public static void Main(string[]args)
+        {
+            int StartPosition   = 0;
+
+            {
+                Console.WriteLine("Welcome to the Snake and Ladder Game");
+                Console.WriteLine("....................................");
+                Console.WriteLine(" The Start Position of The Player is : {0}", StartPosition);
+            }
+        }
+    }
+
+}
+
